4816a8d3e251feb0d8d6b0c1611fd20a76099906
Alison Lee <alee17@stuy.edu>
2017-05-18 21:44:02 -0400

    again

--- ActionCard.java
+++ ActionCard.java
@@ -27,11 +27,17 @@ public class ActionCard extends Card {
 		/* i think swapping while moving towards middle would be faster bc that is n/2 times like:
 			[1] 2 3 4 [5]  : swap nums in boxes
 			5 [2] 3 [4] 1  and so on until you reach middle
+		   how we reverse it doesnt rlly matter though
 		*/
 		for( n = 0 ; n < _players.size() - 1 ; n ++ ) {
 			_players.add( 0, _players.remove( n + 1 )) ;
 		}
-		
+		/* example of reverse algo running vvv
+		1 2 3 4 5
+		1 [2] 3 4 5  : boxed num (at index 1, which is n + 1) will be removed and added to front
+		2 1 [3] 4 5  boxed num is at index 2 
+		3 2 1 [4] 5 and so on
+		*/
     }
     
     public void skip(){
